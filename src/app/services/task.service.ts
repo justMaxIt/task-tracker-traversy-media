cef6ab66067dccd8366291ec87591d9f915232ef
Maksym Fedorenko <justmaksit@gmail.com>
2021-06-01 16:26:22 +0300

    added http simulation API (db.json)

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable, of } from 'rxjs'
 import { Task } from '../Task'
 import { TASKS } from '../mock-tasks'
@@ -6,8 +7,9 @@ import { TASKS } from '../mock-tasks'
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
     const tasks = of(TASKS)
